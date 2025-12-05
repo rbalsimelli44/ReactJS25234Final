f066255fd3620ebedea8fee5ba011af27b7cc170
Ricardo Balsimelli <rbalsimelli@yahoo.com.ar>
2025-12-05 20:14:11 -0300

    third commit

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
 import Icon from '/src/assets/icons/Icon.jsx';
 
-import '../../styles/Navbar.css';
+import '../../styles/navbar.css';
 
 export default function Navbar({ setNavAbierto }) {
   
